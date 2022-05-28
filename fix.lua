6b35fccc68514ae4042fcdf0804d0ab2591e3010
Alleexxi <66967403+Alleexxi@users.noreply.github.com>
2022-05-28 05:41:21 +0200

    Create fix.lua

---
+++ fix.lua
@@ -0,0 +1,6 @@
+if _G.UILOADSTRINGFIX == nil then
+  _G.UILOADSTRINGFIX = true
+  loadstring(dx9.Get("https://raw.githubusercontent.com/soupg/DXLib/main/main.lua"))()
+  loadstring(dx9.Get("https://raw.githubusercontent.com/soupg/supg_ui/main/ui.lua"))()
+
+end
