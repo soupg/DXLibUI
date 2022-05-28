1e06fc66c265e273307587b3073282f18418cf9a
Alleexxi <66967403+Alleexxi@users.noreply.github.com>
2022-05-28 05:43:34 +0200

    Delete fix.lua

--- fix.lua
+++
@@ -1,6 +0,0 @@
-if _G.UILOADSTRINGFIX == nil then
-  _G.UILOADSTRINGFIX = true
-  loadstring(dx9.Get("https://raw.githubusercontent.com/soupg/DXLib/main/main.lua"))()
-  loadstring(dx9.Get("https://raw.githubusercontent.com/soupg/supg_ui/main/ui.lua"))()
-
-end
